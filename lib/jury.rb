d3a0881c03e36d889d782857813d74cb6bf09c40
John Shea <jscruffy@hotmail.com>
2016-02-27 09:57:08 -0500

    Implement class Jury

---
+++ lib/jury.rb
@@ -0,0 +1,47 @@
+class Jury
+  attr_accessor :members
+
+  def initialize
+    @members = Array.new
+    @finalists = Hash.new
+  end
+
+  def announce_winner(final_votes)
+    current_highest_vote_count = 0
+    current_winner = nil
+
+    final_votes.each do |key, value|
+      if value > current_highest_vote_count
+        current_highest_vote_count = value
+        current_winner = key
+      end
+    end
+
+    return current_winner
+
+  end
+
+  def report_votes(final_votes)
+    final_votes.each do |k, v|
+      puts "#{k} got #{v} votes"
+    end
+  end
+
+  def cast_votes(finalists)
+    finalists.each do |finalist|
+      @finalists[finalist] = 0
+    end
+
+    @members.each do |member|
+      random_finalist = @finalists.keys.sample
+      puts "#{random_finalist} gets 1 vote"
+      @finalists[random_finalist] += 1
+    end
+
+    return @finalists
+  end
+
+  def add_member(person)
+    @members << person
+  end
+end
